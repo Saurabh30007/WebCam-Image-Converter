e7d6af4fd60af787f50578daa77f76bdefb226ef
SAM <Sau3007@outlook.com>
2023-09-18 15:35:53 +0530

    Upload Image option added

--- main.py
+++ main.py
@@ -5,6 +5,17 @@
 import streamlit as st
 from PIL import Image
 
+
+# Create a file uploader component allowing the user to upload a file
+upload_image = st.file_uploader('Upload Image')
+
+# Check if the image exists meaning the user has uploaded a file
+if upload_image:
+    up_img = Image.open(upload_image)
+    up_gray_image = up_img.convert('L')
+    st.image(up_gray_image)
+
+
 with st.expander('Start Camera'):
     # Starting the camera
     camera_image = st.camera_input('Camera')
@@ -18,3 +29,6 @@ if camera_image:
 
     # Rendering the grayscale image on the webpage
     st.image(gray_image)
+
+
+# to run this application write 'streamlit run main.py' in the terminal section

0e52ea68bc4de55430c3c03036243da26adecdf3
SAM <Sau3007@outlook.com>
2023-09-18 14:34:52 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a Python script, which takes a picture from the web camera
+# and converts into a gray scale picture
+
+
+import streamlit as st
+from PIL import Image
+
+# Starting the camera
+camera_image = st.camera_input('Camera')
+
+# Creating a pillow image instance
+img = Image.open(camera_image)
+
+# Converting the pillow image in grayscale
+gray_image = img.convert('L')
+
+# Rendering the grayscale image on the webpage
+st.image(gray_image)
